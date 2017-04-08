b79793856e0f8ffbbb571469d7c1529d3fc008e2
Ivan Bryzzhin <brizjin@gmail.com>
2017-04-08 17:49:37 +0300

    get list of unique, locals links to follow on html page

--- tests/test_bs4.py
+++ tests/test_bs4.py
@@ -1,9 +1,9 @@
-import asyncio
 import unittest
 
 import os
+import re
 from bs4 import BeautifulSoup, SoupStrainer
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urldefrag
 
 
 def read_data(file_name):
@@ -15,7 +15,14 @@ class Bs4Test(unittest.TestCase):
         base_url = 'http://python.org'
         html_doc = read_data("python.org.html")
         soup = BeautifulSoup(html_doc, 'lxml', parse_only=SoupStrainer('a'))
-        links = [link.get('href') for link in soup.find_all('a')]
-        absolute_links = [urljoin(base_url, link) for link in links]
-        print("\n".join(sorted(absolute_links)))
+        links = [link.get('href') for link in soup.find_all('a')]           # find all a-tags and get href of it
+        absolute_links = [urljoin(base_url, link) for link in links]        # convert relative links to absolute
         self.assertIsNotNone(absolute_links, "absolute links should not be null")
+        # remove external links, remove fragment from url, distinct links
+        r = re.compile('^' + base_url)
+        # noinspection PyTypeChecker
+        links = set(urldefrag(link)[0].strip('/') for link in filter(r.match, absolute_links))
+        # f = []
+
+        print("\n".join(sorted(links)))
+        print(links)
