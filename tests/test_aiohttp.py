ad5800003f91eb5b5911dd239c42a26d60af81f8
Ivan Bryzzhin <brizjin@gmail.com>
2017-04-09 17:28:42 +0300

    asyncio parse_page

--- tests/test_aiohttp.py
+++ tests/test_aiohttp.py
@@ -1,6 +1,9 @@
+import concurrent
 import unittest
 import asyncio
 import aiohttp
+
+from run_scrapper import parse_html
 
 
 class AsyncioHelloWordTest(unittest.TestCase):
@@ -12,20 +15,30 @@ class AsyncioHelloWordTest(unittest.TestCase):
         self.loop.close()
 
     def test_dumb(self):
-        async def fetch(session, url):
-            with aiohttp.Timeout(10, loop=session.loop):
-                async with session.get(url) as response:
-                    return await response.text()
+        loop = self.loop
+        async def fetch_page(session, url):
+            async with session.get(url) as response:
+                return await response.text()
 
-        async def main(loop):
-            async with aiohttp.ClientSession(loop=loop) as session:
-                html = await fetch(session, 'http://python.org')
-                self.assertIsNotNone(html, "python.org returns null")
-                # print(html)
-                return html
+        async def parse_page(executor, base_url, html_doc):
+            return await loop.run_in_executor(executor, parse_html, base_url, html_doc)
 
-        # loop = asyncio.get_event_loop()
-        # loop.run_until_complete(main(loop))
-        html = self.loop.run_until_complete(main(self.loop))
-        print(html)
+        #async def spy(executor, base_url, html_doc):
+
+
+
+
+        async def main():
+            with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
+                async with aiohttp.ClientSession(loop=loop) as session:
+                    base_url = 'http://python.org'
+                    html_doc = await fetch_page(session, base_url)
+                    links_assets = await parse_page(executor, base_url, html_doc)
+                    print(links_assets[0])
+
+                    # print(html)
+                    return links_assets[0]
+        html = loop.run_until_complete(main())
+        self.assertIsNotNone(html, "python.org returns null")
+        # print(html)
 
